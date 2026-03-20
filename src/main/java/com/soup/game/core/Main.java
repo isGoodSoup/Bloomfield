3e3c77a79f483af6456149875fa7d3daf7dc63e7
diego <sargent.pointed453@passinbox.com>
2026-03-20 10:21:32 +0100

    update: new main class fri 20-03 10:21

--- src/main/java/com/soup/game/core/Main.java
+++ src/main/java/com/soup/game/core/Main.java
@@ -1,7 +1,12 @@
 package com.soup.game.core;
 
 import com.soup.game.swing.SwingFrame;
+import com.soup.game.swing.SwingPanel;
 
+/**
+ * The main method, which executes the SwingFrame window and its
+ * {@link SwingPanel}
+ */
 public class Main {
     public static void main(String[] args) {
         new SwingFrame();
